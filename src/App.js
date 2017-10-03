bf0746d64c45150c4887571473f215dba1f9b6a2
Himesh Jain <himeshjain13@gmail.com>
2017-10-04 02:31:40 +0530

    Create Initial Structure

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Home from './components/Home/Home';
+import AddBusiness from './components/AddBusiness/AddBusiness';
+import StaffDetails from './components/StaffDetails/StaffDetails';
+import ViewPackage from './components/ViewPackage/ViewPackage';
+import { Route } from 'react-router-dom';
+import './css/App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">PlanIt!</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Route exact={true} path="/" component={Home} />
+        <Route path="/add-verify-business" component={AddBusiness} />
+        <Route path="/staff-details" component={StaffDetails} />
+        <Route path="/view-package" component={ViewPackage} />
       </div>
     );
   }
---
+++ src/components/ViewPackage/ViewPackage.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import '../../css/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p className="App-intro">
+          <Link to="/">Back</Link>
+          <br />
+          <br />
+          View Packages Here
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
