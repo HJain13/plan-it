9a0ef641ebcd73e85b1af36be8054bb353cd23ea
Himesh Jain <himeshjain13@gmail.com>
2017-10-31 18:32:07 +0530

    Fix Show Progress in Business Index

--- frontend/src/components/BusinessIndex/BusinessIndex.js
+++ frontend/src/components/BusinessIndex/BusinessIndex.js
@@ -6,6 +6,14 @@ import TableRowBusiness from '../TableRowBusiness';
 
 var baseUrl = '/api';
 
+function IsLoading() {
+  return (
+    <div className="is-material-progress">
+      <div className="indeterminate"></div>
+    </div>
+  );
+}
+
 class BusinessIndex extends Component {
  constructor(props) {
     super(props);
@@ -24,7 +32,7 @@ class BusinessIndex extends Component {
         approved: 'false'
       },
       items: '',
-      isLoading: true
+      is_loading: true
     };
     this.addBusinessService = new BusinessService();
   }
@@ -37,54 +45,48 @@ class BusinessIndex extends Component {
       .catch(function (error) {
         console.log(error);
       })
-    
-    this.setState({isLoading: false}); 
   }
   tabRow() {
     if (this.state.businesses instanceof Array) {
+      this.setState({is_loading: false}); 
       return this
         .state
         .businesses
         .map(function (object, i) {
           return <TableRowBusiness obj={object} key={i}/>;
-
         })
-    }
+      }
   }
 
   render() {
     if (localStorage.isLoggedIn === 'yes' && localStorage.userType === 'admin') {
-      if(this.state.isLoading) {
-
-      }
-      else {
-        return (
-          <div>
-            <Header location={this.props.location} />
-            <div className="hero is-light">
-              <div className="hero-body">
-                <div className="container has-text-centered">
-                  <table className="table is-striped">
-                    <thead>
-                      <tr>
-                        <td>Business ID</td>
-                        <td>Name</td>
-                        <td>Email</td>
-                        <td>Phone No.</td>
-                        <td>Is Approved</td>
-                        <td></td>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {this.tabRow()}
-                    </tbody>
-                  </table>
-                </div>
+      return (
+        <div>
+          { this.state.is_loading && <IsLoading /> }            
+          <Header location={this.props.location} />
+          <div className="hero is-light">
+            <div className="hero-body">
+              <div className="container has-text-centered">
+                <table className="table is-striped">
+                  <thead>
+                    <tr>
+                      <td>Business ID</td>
+                      <td>Name</td>
+                      <td>Email</td>
+                      <td>Phone No.</td>
+                      <td>Is Approved</td>
+                      <td></td>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {this.tabRow()}
+                  </tbody>
+                </table>
               </div>
             </div>
           </div>
-        );
-      }
+        </div>
+      );
     }
     else {
       localStorage.isLoggedIn="false";
