ef4e90c4f16920400ccc2ce6e3e8292688a670af
Yash Nag <hsaygan@gmail.com>
2017-11-22 20:42:26 +0530

    Updated (not Complete) builddb.js

--- build-db.js
+++ build-db.js
@@ -22,7 +22,7 @@ if (process.env.environment === 'development') {
   mongoUrl = 'mongodb://localhost:27016/plan-it';
 }
 else {
-  mongoUrl = process.env.mongoUrl;  
+  mongoUrl = process.env.mongoUrl;
 }
 // Mongoose connection with MongoDB
 mongoose.connect(mongoUrl, {
@@ -33,11 +33,11 @@ mongoose.connect(mongoUrl, {
   console.log('');
   console.log('=======================================');
   console.log('Creating Database Entries:');
-  console.log('=======================================');  
+  console.log('=======================================');
   console.log('');
   //=========================================
   //               Admin Tasks
-  //========================================= 
+  //=========================================
   function buildAdmin() {
     count++;
     return new Promise((resolve, reject) => {
@@ -53,14 +53,14 @@ mongoose.connect(mongoUrl, {
       .then( () => {
         //Adding an Admin to System
         var admins = new Array(2);
-        admins[0] = new Admin({"admin": { "phone_no": "7894561203", "repass": "741852", "pass": "741852", "email": "15ucs173@lnmiit.ac.in", "name": "Yash Nag", "u_type": "admin"}});
+        admins[0] = new Admin({"admin": { "phone_no": "7894561203", "repass": "temp1234", "pass": "temp1234", "email": "15ucs173@lnmiit.ac.in", "name": "Yash Nag", "u_type": "admin"}});
         admins[1] = new Admin({"admin": { "phone_no": "7894561203", "repass": "qwerty", "pass": "qwerty", "email": "15ucs172@lnmiit.ac.in", "name": "Himesh Jain", "u_type": "admin"}});
 
         admins.forEach(function (admin, index, array) {
           admin.save().then(admin => {
               console.log('+++ Admin['+index+'] added successfully +++');
               resolve(admin);
-              count++;          
+              count++;
             })
           .catch(err => {
             console.error("Unable to save to database: ", err.stack);
@@ -70,10 +70,10 @@ mongoose.connect(mongoUrl, {
       resolve(count);
     });
   }
-  
+
   //=========================================
   //               Business Tasks
-  //========================================= 
+  //=========================================
   function buildBusiness() {
     count++;
     return new Promise((resolve, reject) => {
@@ -90,16 +90,11 @@ mongoose.connect(mongoUrl, {
       .then(() => {
         //Adding an Business to System
         var businesses = new Array(10);
-        businesses[0] = new Business({"business": {"name": "Tagchat","m_name": "Lona Dany","btype": "dining","email": "ldany0@nhs.uk","pass": "3YMRneG2","repass": "3YMRneG2","b_ac_no": "374622008407594","address": "50063 Monica Way","phone_no": "+86 191 679 5817","u_type": "business","approved": "false"}});
-        businesses[1] = new Business({"business": {"name": "BlogXS","m_name": "Dale Cheson","btype": "activity","email": "dcheson1@yale.edu","pass": "2JHqUekvru","repass": "2JHqUekvru","b_ac_no": "30218430182123","address": "7441 Dunning Plaza","phone_no": "+48 660 537 1950","u_type": "business","approved": "true"}});    
-        businesses[2] = new Business({"business": {"name": "Brainbox","m_name": "Janka Haddrill","btype": "dining","email": "jhaddrill2@comcast.net","pass": "doMzfM","repass": "doMzfM","b_ac_no": "5602222542010328","address": "022 Warrior Point","phone_no": "+1 917 175 8259","u_type": "business","approved": "false"}});    
-        businesses[3] = new Business({"business": {"name": "Kwimbee","m_name": "Xymenes Bygate","btype": "activity","email": "xbygate3@aol.com","pass": "5inim3bqw1","repass": "5inim3bqw1","b_ac_no": "3544128315024915","address": "92068 8th Road","phone_no": "+93 753 919 6792","u_type": "business","approved": "true"}});    
-        businesses[4] = new Business({"business": {"name": "DabZ", "m_name": "Darcey Delouch", "btype": "activity", "email": "ddelouch4@i2i.jp", "pass": "8uLWWRo9dB", "repass": "8uLWWRo9dB", "b_ac_no": "5134519731265630", "address": "256 Pleasure Court", "phone_no": "+230 763 465 1805", "u_type": "business", "approved": "true"}});    
-        businesses[5] = new Business({"business": {"name": "Roodel", "m_name": "Carolin Farran", "btype": "dining", "email": "cfarran9@bing.com", "pass": "BlATCk16gM", "repass": "BlATCk16gM", "b_ac_no": "3588850746032988", "address": "2899 Barby Drive", "phone_no": "+51 891 587 2765", "u_type": "business", "approved": "flase"}});    
-        businesses[6] = new Business({"business": {"name": "Twinder", "m_name": "Susana Cosin", "btype": "travel", "email": "scosin8@examiner.com", "pass": "dJzVExeRV", "repass": "dJzVExeRV", "b_ac_no": "3576445880295360", "address": "393 Manitowish Pass", "phone_no": "+86 581 922 4109", "u_type": "business", "approved": "true"}});    
-        businesses[7] = new Business({"business": {"name": "Shuffledrive", "m_name": "Hamil Cabrara", "btype": "dining", "email": "hcabrara7@jimdo.com", "pass": "DNEbrF6yW", "repass": "DNEbrF6yW", "b_ac_no": "3587858521763397", "address": "2937 Coleman Drive", "phone_no": "+63 964 648 2108", "u_type": "business", "approved": "false"}});    
-        businesses[8] = new Business({"business": {"name": "Skidoo", "m_name": "Urson Manning", "btype": "dining", "email": "umanning6@aol.com", "pass": "KJzvhrEO", "repass": "KJzvhrEO", "b_ac_no": "30067895485848", "address": "5983 Hanson Road", "phone_no": "+856 100 914 2931", "u_type": "business", "approved": "false"}});    
-        businesses[9] = new Business({"business": {"name": "Vinder", "m_name": "Nikolaos Lascelles", "btype": "travel", "email": "nlascelles5@google.pl", "pass": "kbFppsiskRbm", "repass": "kbFppsiskRbm", "b_ac_no": "4026230823754465", "address": "5094 Lakewood Terrace", "phone_no": "+507 890 686 4971", "u_type": "business", "approved": "true"}});    
+        businesses[0] = new Business({"business": {"name": "Kwimbee", "bemail": "manager@Kwimbee.com", "password": "abcd1234", "address": "167 Malviya Nagar, Jaipur", "bank_acc": "30218430182121", "manager_name": "Abhishek Agarwal", "phone_no": "+91 9812736452", "approved": "false"}});
+        businesses[1] = new Business({"business": {"name": "DabZ", "bemail": "manager@DabZ.com", "password": "abcd1234", "address": "15/89 Mansarovar, Jaipur", "bank_acc": "19378430182129", "manager_name": "Yash Sharma", "phone_no": "+91 8174623512", "approved": "false"}});
+        businesses[2] = new Business({"business": {"name": "Roodel", "bemail": "manager@Roodel.com", "password": "abcd1234", "address": "A012 Shantanu Nagar, Jaipur", "bank_acc": "49188430182123", "manager_name": "Varun Goyal", "phone_no": "+91 8762384712", "approved": "false"}});
+        businesses[3] = new Business({"business": {"name": "Roodel", "bemail": "manager@Roodel.com", "password": "abcd1234", "address": "188/230 Transport Nagar, Jaipur", "bank_acc": "10298430182120", "manager_name": "Avinash Mathur", "phone_no": "+91 9273648162", "approved": "false"}});
+        businesses[4] = new Business({"business": {"name": "TomoNow", "bemail": "manager@TomoNow.com", "password": "abcd1234", "address": "10 Arravali Nagar, Jaipur", "bank_acc": "31294301821232", "manager_name": "Riya Shrivastava", "phone_no": "+91 9876253614", "approved": "false"}});
         businesses.forEach(function (business, index, array) {
           business.save().then(business => {
               console.log('+++ Business['+index+'] added successfully +++');
@@ -133,16 +128,11 @@ mongoose.connect(mongoUrl, {
       .then(() => {
         //Adding an User to System
         var users = new Array(10);
-        users[0] = new User({"user": {"name": "Tagchat","m_name": "Lona Dany","btype": "dining","email": "ldany0@nhs.uk","pass": "3YMRneG2","repass": "3YMRneG2","b_ac_no": "374622008407594","address": "50063 Monica Way","phone_no": "+86 191 679 5817","u_type": "user","approved": "false"}});
-        users[1] = new User({"user": {"name": "BlogXS","m_name": "Dale Cheson","btype": "activity","email": "dcheson1@yale.edu","pass": "2JHqUekvru","repass": "2JHqUekvru","b_ac_no": "30218430182123","address": "7441 Dunning Plaza","phone_no": "+48 660 537 1950","u_type": "user","approved": "true"}});    
-        users[2] = new User({"user": {"name": "Brainbox","m_name": "Janka Haddrill","btype": "dining","email": "jhaddrill2@comcast.net","pass": "doMzfM","repass": "doMzfM","b_ac_no": "5602222542010328","address": "022 Warrior Point","phone_no": "+1 917 175 8259","u_type": "user","approved": "false"}});    
-        users[3] = new User({"user": {"name": "Kwimbee","m_name": "Xymenes Bygate","btype": "activity","email": "xbygate3@aol.com","pass": "5inim3bqw1","repass": "5inim3bqw1","b_ac_no": "3544128315024915","address": "92068 8th Road","phone_no": "+93 753 919 6792","u_type": "user","approved": "true"}});    
-        users[4] = new User({"user": {"name": "DabZ", "m_name": "Darcey Delouch", "btype": "activity", "email": "ddelouch4@i2i.jp", "pass": "8uLWWRo9dB", "repass": "8uLWWRo9dB", "b_ac_no": "5134519731265630", "address": "256 Pleasure Court", "phone_no": "+230 763 465 1805", "u_type": "user", "approved": "true"}});    
-        users[5] = new User({"user": {"name": "Roodel", "m_name": "Carolin Farran", "btype": "dining", "email": "cfarran9@bing.com", "pass": "BlATCk16gM", "repass": "BlATCk16gM", "b_ac_no": "3588850746032988", "address": "2899 Barby Drive", "phone_no": "+51 891 587 2765", "u_type": "user", "approved": "flase"}});    
-        users[6] = new User({"user": {"name": "Twinder", "m_name": "Susana Cosin", "btype": "travel", "email": "scosin8@examiner.com", "pass": "dJzVExeRV", "repass": "dJzVExeRV", "b_ac_no": "3576445880295360", "address": "393 Manitowish Pass", "phone_no": "+86 581 922 4109", "u_type": "user", "approved": "true"}});    
-        users[7] = new User({"user": {"name": "Shuffledrive", "m_name": "Hamil Cabrara", "btype": "dining", "email": "hcabrara7@jimdo.com", "pass": "DNEbrF6yW", "repass": "DNEbrF6yW", "b_ac_no": "3587858521763397", "address": "2937 Coleman Drive", "phone_no": "+63 964 648 2108", "u_type": "user", "approved": "false"}});    
-        users[8] = new User({"user": {"name": "Skidoo", "m_name": "Urson Manning", "btype": "dining", "email": "umanning6@aol.com", "pass": "KJzvhrEO", "repass": "KJzvhrEO", "b_ac_no": "30067895485848", "address": "5983 Hanson Road", "phone_no": "+856 100 914 2931", "u_type": "user", "approved": "false"}});    
-        users[9] = new User({"user": {"name": "Vinder", "m_name": "Nikolaos Lascelles", "btype": "travel", "email": "nlascelles5@google.pl", "pass": "kbFppsiskRbm", "repass": "kbFppsiskRbm", "b_ac_no": "4026230823754465", "address": "5094 Lakewood Terrace", "phone_no": "+507 890 686 4971", "u_type": "user", "approved": "true"}});    
+        users[0] = new User({"user": {"name": "Chaitanya Maheshwari", "email": "manager@Vinder.com", "password": "abcd1234", "address": "88/23 Triveni Nagar, Jaipur", "bank_acc": "49498430182123", "manager_name": "Vatsla Singh", "phone_no": "+91 9166002847", "approved": "false"}});
+        users[1] = new User({"user": {"name": "BlogXS","m_name": "Dale Cheson","btype": "activity","email": "dcheson1@yale.edu","pass": "2JHqUekvru","repass": "2JHqUekvru","b_ac_no": "30218430182123","address": "7441 Dunning Plaza","phone_no": "+48 660 537 1950","u_type": "user","approved": "true"}});
+        users[2] = new User({"user": {"name": "Brainbox","m_name": "Janka Haddrill","btype": "dining","email": "jhaddrill2@comcast.net","pass": "doMzfM","repass": "doMzfM","b_ac_no": "5602222542010328","address": "022 Warrior Point","phone_no": "+1 917 175 8259","u_type": "user","approved": "false"}});
+        users[3] = new User({"user": {"name": "Kwimbee","m_name": "Xymenes Bygate","btype": "activity","email": "xbygate3@aol.com","pass": "5inim3bqw1","repass": "5inim3bqw1","b_ac_no": "3544128315024915","address": "92068 8th Road","phone_no": "+93 753 919 6792","u_type": "user","approved": "true"}});
+        users[4] = new User({"user": {"name": "DabZ", "m_name": "Darcey Delouch", "btype": "activity", "email": "ddelouch4@i2i.jp", "pass": "8uLWWRo9dB", "repass": "8uLWWRo9dB", "b_ac_no": "5134519731265630", "address": "256 Pleasure Court", "phone_no": "+230 763 465 1805", "u_type": "user", "approved": "true"}});
         users.forEach(function (user, index, array) {
           user.save().then(user => {
               console.log('+++ User['+index+'] added successfully +++');
@@ -158,7 +148,7 @@ mongoose.connect(mongoUrl, {
         resolve(count);
       });
     });
-  }  
+  }
 
   function buildPackage() {
     count++;
@@ -180,10 +170,10 @@ mongoose.connect(mongoUrl, {
         packages[1] = new Package({"package" : {"name": "Hotel Ramada", "brochure":"I am in Pain", "location":"Jaipur", "activity":"Swimming", "cost":"400", "bemail":"ldany0@nhs.uk", "ptype":"activity"}});
         packages[2] = new Package({"package" : {"name": "Hotel Ramada", "brochure":"I am in Pain", "location":"Jaipur", "activity":"Swimming","food_specials": [ "fish cury", " thai food" ], "duration":"6 days", "cost":"400", "pictures": [ "https://brightcove04pmdo-a.akamaihd.net/5104226627001/5104226627001_5230061447001_5203619434001-vs.jpg?pubId=5104226627001&videoId=5203619434001" ],"ptype":"travel"}});
         packages[3] = new Package({"package" : {"name": "Hotel Ramada", "brochure":"I am in Pain", "location":"Jaipur", "activity":"Swimming","food_specials": [ "fish cury", " thai food" ], "duration":"6 days", "cost":"400", "pictures": [ "https://c1.hiqcdn.com/uploadimages/travel/Jaipur_irctc_cover.jpg" ],"ptype":"travel"}});
-        
+
         packages[4] = new Package({"package" :  { "combo_name": "Chotta packet badda dhamaka", "menu_image":"https://s3-ap-southeast-1.amazonaws.com/magicpin-selfie/customerimages/11386_food_1_app.jpg", "specials": [ "Grilles cheese sandwich", "roasted chicken" ], "pictures":[ "https://drop.ndtv.com/albums/COOKS/pasta-vegetarian/pastaveg_640x480.jpg" ], "cost_for_two": '2500', "ptype": "dining" }});
-        
-        packages[5] = new Package({ "package": 
+
+        packages[5] = new Package({ "package":
           { "combo_name": "fdasfa",
             "menu_image": "https://static.pexels.com/photos/46239/salmon-dish-food-meal-46239.jpeg",
             "specials": [ "dafa" ],
@@ -195,7 +185,7 @@ mongoose.connect(mongoUrl, {
         packages[6] = new Package({ "package":{ "name": "Water water everywhere", "brochure": "Engage your child in the hunt.", "location": "Rishikash", "activity": [ "River Rafting", "treking", " cliff Jumping" ], "cost": "2500", "ptype": "activity" }});
 
          packages[7] = new Package({ "package":{"brochure": "Explore the world","location": "maylasia", "activity": ["Scuba diving", " Aquarium Visit" ], "food_specials": [ "fish cury", " thai food" ], "duration":"6 days", "cost": "50,000",  "pictures": [ "https://img.grouponcdn.com/deal/32pQynDNtNyUVmDgnibucUstWnmN/32-2048x1229/v1/c700x420.jpg" ], "ptype": "travel" } });
-       
+
         packages.forEach(function (package, index, array) {
           package.save().then(package => {
               console.log('+++ Package['+index+'] added successfully +++');
@@ -211,7 +201,7 @@ mongoose.connect(mongoUrl, {
         resolve(count);
       });
     });
-  }  
+  }
 
 
   buildAdmin()
@@ -219,9 +209,9 @@ mongoose.connect(mongoUrl, {
     buildBusiness()
     .then( () => {
       buildUser()
-      .then( () => {   
+      .then( () => {
         buildPackage()
-        .then( () => {              
+        .then( () => {
           mongoose.connection.close();
         })
       })
